71dcef3798b9472885853442bda75e59769ad3e0
wangshuai <1992149090@qq.com>
2017-09-25 00:14:21 +0800

    去掉防止多击事件的处理

--- butterknife/src/main/java/butterknife/internal/DebouncingOnClickListener.java
+++ butterknife/src/main/java/butterknife/internal/DebouncingOnClickListener.java
@@ -7,21 +7,23 @@ import android.view.View;
  * same frame. A click on one button disables all buttons for that frame.
  */
 public abstract class DebouncingOnClickListener implements View.OnClickListener {
-  static boolean enabled = true;
+    static boolean enabled = true;
 
-  private static final Runnable ENABLE_AGAIN = new Runnable() {
-    @Override public void run() {
-      enabled = true;
+    private static final Runnable ENABLE_AGAIN = new Runnable() {
+        @Override
+        public void run() {
+            enabled = true;
+        }
+    };
+
+    @Override
+    public final void onClick(View v) {
+        //    if (enabled) {
+        //      enabled = false;
+        //      v.post(ENABLE_AGAIN);
+        doClick(v);
+        //    }
     }
-  };
 
-  @Override public final void onClick(View v) {
-    if (enabled) {
-      enabled = false;
-      v.post(ENABLE_AGAIN);
-      doClick(v);
-    }
-  }
-
-  public abstract void doClick(View v);
+    public abstract void doClick(View v);
 }
